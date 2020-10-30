3f5107c866d522c6bf49548ff6c24b2c276823ef
eberj <eberj@DESKTOP-4SH6NRH>
2020-10-29 20:46:19 -0500

    assigned array and stored back, then let total = total sales and display it  back

---
+++ Unit4HW7/public_html/Unit4HW7.js
@@ -0,0 +1,18 @@
+/* 7. Write a program that asks the user to enter a store's sales for each day
+ *  of the week.
+The amounts should be stored in an array. 
+Use a loop to calculate the total sales for the week and display the results.
+ */
+
+
+
+let sales = [];
+while(sales.length < 6){
+    sales.push(Number(prompt("Enter number ")));
+}
+
+let total = 0;
+for(let sale of sales){
+    total += sale;
+}
+document.write("Weekly sales- " + sales + "<br>" + "Total sales- " + total);
